962adb2854cfafc2c787787df37f4a28190c0bc7
Muhammed Swalih <swalihmuhammed120@gmail.com>
2026-08-01 20:20:12 +0530

    Update init.database.sql

--- scripts/init.database.sql
+++ scripts/init.database.sql
@@ -22,7 +22,7 @@ GO
 
 -- Drop and recreate 'DataWarehouse' databse
 
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehose')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
 	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 	DROP DATABASE DataWarehouse
